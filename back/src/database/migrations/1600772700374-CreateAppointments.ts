495b60246026b0264aa9b3df93625a57677e1eef
Wilson Neto <contato@wilsonneto.com.br>
2020-09-22 08:06:12 -0300

    database++

---
+++ back/src/database/migrations/1600772700374-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1600772700374 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
